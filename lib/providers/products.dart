21ea75a8f17c5a2f5b87198cb21d11bb17425336
Amir <amir.husain@getfareye.com>
2022-02-22 11:22:02 +0530

    provider added

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../models/product.dart';
+import '../providers/product.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = [
@@ -56,6 +56,10 @@ class Products with ChangeNotifier {
 
   List<Product> get items {
     return [..._items];
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
   }
 
   void addProduct() {
